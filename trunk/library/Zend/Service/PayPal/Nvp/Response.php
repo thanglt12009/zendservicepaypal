0749a781f0f45bbc5bb04ff031ecb8c67c95aa0d
rokclimb15 <rokclimb15@b395c14c-a63b-11de-a118-c7387b1e4f66>
2009-11-24 18:03:48 +0000

    Implement Adapter/Factory pattern.  Throw exceptions for Http errors.  Add new API methods.

--- trunk/library/Zend/Service/PayPal/Nvp/Response.php
+++ trunk/library/Zend/Service/PayPal/Nvp/Response.php
@@ -25,10 +25,10 @@ class Zend_Service_PayPal_Nvp_Response extends Zend_Service_PayPal_Response
      * @param Zend_Http_Response $response 
      * 
      */
-    public function __construct( $response )
+    public function __construct($response)
     {
         $this->_response = $response;
-        parse_str( $response->getBody(), $this->_data );
+        parse_str($response->getBody(), $this->_data);
     }
     
     /**
@@ -38,19 +38,11 @@ class Zend_Service_PayPal_Nvp_Response extends Zend_Service_PayPal_Response
      */
     public function isSuccess()
     {
-
-        if( !$this->_response->isSuccessful() ) {
-            return false;
-        }
-
-        if( !isset( $this->_data['ACK'] ) )
-        {
-            return false;
-        }
-        
-        if( substr( $this->_data['ACK'], 0 , 7 ) == 'Success' ) {
+        if (isset($this->_data['ACK']) && substr($this->_data['ACK'], 0, 7) == 'Success') {
             return true;
         }
+        
+        return false;
     }
 
     /**
@@ -60,31 +52,13 @@ class Zend_Service_PayPal_Nvp_Response extends Zend_Service_PayPal_Response
      */
     public function isError()
     {
-        if( !isset( $this->_data['ACK'] ) ) {
+        if (! isset($this->_data['ACK'])) {
             return false; 
         }
 
-        if( substr( $this->_data['ACK'], 0, 5 ) == 'Error' ) {
+        if (substr($this->_data['ACK'], 0, 7) == 'Failure') {
             return true;
         }
-    }
-
-    /**
-     * TODO: short description.
-     * 
-     * @return boolean
-     */
-    public function isFailure()
-    {
-        if( !$this->_response->isSuccessful() ) {
-            return true;
-        }
-
-        if( !isset( $this->_data['ACK'] ) ) {
-            return true;
-        }
-
-        return false;
     }
 
     /**
@@ -92,14 +66,11 @@ class Zend_Service_PayPal_Nvp_Response extends Zend_Service_PayPal_Response
      * 
      * @return string
      */
-    public function getValue( $key )
+    public function getValue($key)
     {
         $key = strtoupper($key);
-        if( isset( $this->_data[$key] ) ) {
-            return $this->_data[$key];
-        }
-
-        return null;
+        
+        return parent::getValue($key);
     }
 
     /**
